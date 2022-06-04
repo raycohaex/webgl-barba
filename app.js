656331337a5a107df2d36bae8a04165177d90b5a
Rayco Haex <raycoh1299@gmail.com>
2022-06-04 21:05:50 +0200

    shader experiment

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import fragment from './shaders/fragment.glsl';
+import vertex from './shaders/vertex.glsl';
 
 // init
 
@@ -10,11 +12,14 @@ export default class Sketch {
         this.height = this.container.offsetHeight;
 
         this.camera = new THREE.PerspectiveCamera( 70, this.width / this.height, 0.01, 10 );
-        this.camera.position.z = 1;
+        this.camera.position.z = 3;
         
         this.scene = new THREE.Scene();
         
-        this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+        this.renderer = new THREE.WebGLRenderer( { 
+            antialias: true,
+            alpha: true
+        } );
         this.renderer.setPixelRatio(window.devicePixelRatio);
 
         this.container.appendChild( this.renderer.domElement );
@@ -41,14 +46,24 @@ export default class Sketch {
     }
 
     addObjects() {
-        this.geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-        this.material = new THREE.MeshNormalMaterial();
+        this.geometry = new THREE.SphereBufferGeometry( 0.99, 80,80 );
+
+        this.material = new THREE.ShaderMaterial( {
+            uniforms: { // pass uniforms to shader
+                time: { value: 1.0 },
+                resolution: {value: new THREE.Vector2() }
+            },
+            vertexShader: vertex,
+            fragmentShader: fragment,
+        })
         
         this.mesh = new THREE.Mesh( this.geometry, this.material );
         this.scene.add( this.mesh );
     }
 
     render() {
+        this.time += 0.01;
+        this.material.uniforms.time.value = this.time;
         this.mesh.rotation.x = this.time / 2000;
         this.mesh.rotation.y = this.time / 1000;
     
